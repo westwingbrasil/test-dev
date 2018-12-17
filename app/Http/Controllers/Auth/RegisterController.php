8fba3ca2bfb6c56d0598a1c3c9018815d44f05d2
geekcom <danielrodrigues-ti@hotmail.com>
2018-12-17 14:23:30 -0200

    fix: class App/Models/User

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -63,7 +63,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        return \App\Models\User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
