2baac6ba18c8b225278cff113086f17249bfc588
FranBriao <fran_sb@outlook.com>
2019-08-17 20:18:38 -0300

    Add Migrations. Remove Auth. Fix Env.
    - Migrations files were generated and modified.
    - Auth were removed, as it isn't asked for on the test specs.
    - Env file fixed. Database wasn't able to connect, fixed changing host from 127.0.0.1 to MySQL container's name.

---
+++ src/database/migrations/2019_08_17_211833_create_tickets_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('orderId')->unsigned();
+            $table->string('customerName');
+            $table->string('subject');
+            $table->text('message');
+            $table->date('createDate');
+            $table->timestamps();
+
+            $table->foreign('orderId')->references('id')->on('orders');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
