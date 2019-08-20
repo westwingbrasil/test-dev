ed45e43247f5fc7f1078c0a500500093e059d8a2
Daniel Resnitzky <dcresnitzky@gmail.com>
2019-08-20 14:22:12 -0300

    finishing scripts

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ git submodule update --init laradock
 cd ./laradock
 cp env-example .env
 docker-compose up -d mysql
-docker-compose exec mysql mysql -u root -proot -e "alter user 'default'@'%' identified with mysql_native_password by 'secret';"
 docker-compose up composer
+docker-compose exec mysql mysql -u root -proot -e "alter user 'default'@'%' identified with mysql_native_password by 'secret';"
+docker-compose up -d php-fpm
 docker-compose exec php-fpm php artisan migrate
