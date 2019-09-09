5d65a686edd15861b3b2f5fad7564eb1198a9f89
Felipe Ruiz <fgbruiz@gmail.com>
2019-09-08 23:29:33 -0300

    Seed database with a few values

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,44 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $user_orders = [];
+        for ($i = 1; $i <= 5; ++$i) {
+            $user_id = DB::table('users')->insertGetId([
+                'name' => 'Example '.$i,
+                'email' => 'example'.$i.'@example.com'
+            ], 'user_id');
+
+            $max_orders = rand(1, 3);
+            for ($j = 0; $j < $max_orders; ++$j) {
+                $user_orders[] = [
+                    'user_id' => $user_id,
+                    'user_name' => 'Example '.$i,
+                ];
+            }
+        }
+
+        shuffle($user_orders);
+        foreach ($user_orders as $idx => &$order) {
+            $order_id = $idx + 1;
+            $order['order_id'] = $order_id;
+
+            DB::table('orders')->insert([
+                'order_id' => $order['order_id'],
+                'user_id' => $order['user_id']
+            ]);
+        }
+        unset($order);
+
+        $num_tickets = ceil(count($user_orders) * 2 / 3);
+        $orders_with_tickets = array_rand($user_orders, $num_tickets);
+        foreach ($orders_with_tickets as $i) {
+            $order = $user_orders[$i];
+            DB::table('tickets')->insert([
+                'order_id' => $order['order_id'],
+                'subject' => 'Ticket subject U'.$order['user_id'].'O'.$order['order_id'],
+                'body' => 'This is my ticket for order '.$order['order_id'].', my name is '.$order['user_name'],
+                'created_at' => DB::raw('(SELECT DATE_ADD(CURRENT_TIMESTAMP, INTERVAL FLOOR(RAND() * 121) SECOND))')
+            ]);
+        }
     }
 }
